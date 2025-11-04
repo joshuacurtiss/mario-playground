964d8e98d3da9c39890c7a0a8f08e95674f0339b
Josh Curtiss <josh@curtiss.me>
2025-11-04 01:40:55 -0600

    feat: Add basic camera support

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -3,11 +3,12 @@ import { makePlayer } from '../entities/player';
 
 const scale=4;
 const spriteSize=16;
+const landSpriteCount = 50;
 
 export default function() {
    k.setGravity(3100);
    // Ground
-   for( let i=0 ; i<k.width()/scale/spriteSize ; i++ ) {
+   for( let i=0 ; i<landSpriteCount ; i++ ) {
       k.add([
          k.sprite('items', { anim: 'grass' }),
          k.scale(scale),
@@ -15,18 +16,19 @@ export default function() {
       ]);
    }
    k.add([
+      k.rect((landSpriteCount+2)*spriteSize*scale, spriteSize*scale),
+      k.opacity(0),
+      k.pos(-spriteSize*scale, k.height()-(spriteSize-2)*scale),
+      k.area(),
+      k.body({ isStatic: true }),
+   ]);
+   const ui = k.add([ k.fixed() ]);
+   ui.add([
       k.text('Use Arrow Keys to Move, Shift to Run, Z to Jump', { size: 24,  align: 'center', width: k.width() }),
       k.pos(0, 20),
       k.opacity(0.4),
    ])
-   k.add([
-      k.rect(k.width()*1.2, spriteSize*scale),
-      k.opacity(0),
-      k.pos(-k.width()/10, k.height()-(spriteSize-2)*scale),
-      k.area(),
-      k.body({ isStatic: true }),
-   ]);
-   const playerDebugText = debug ? k.add([
+   const playerDebugText = debug ? ui.add([
       k.text('', { size: 16, width: k.width()/2, lineSpacing: 3 }),
       k.anchor('topleft'),
       k.pos(10, 50),
@@ -36,9 +38,8 @@ export default function() {
       debugText: playerDebugText,
    });
    k.onUpdate(() => {
-      if (player.isOffScreen()) {
-         if (player.pos.x<0) player.moveTo(k.width(), player.pos.y);
-         else player.moveTo(0, player.pos.y);
-      }
+      k.setCamPos(k.getCamPos().lerp(k.vec2(player.pos.x, k.height()/2), 0.08));
+      if (player.pos.x<0) player.moveTo(0, player.pos.y);
+      if (player.pos.x>landSpriteCount*scale*spriteSize) player.moveTo(landSpriteCount*scale*spriteSize, player.pos.y);
    });
 };
