e85e2826fb21523b5f5752cb1f549080c4e4a3e5
Josh Curtiss <josh@curtiss.me>
2025-11-05 02:12:15 -0600

    feat: Improve the player's collision detection and abilities
    - Ability to get grow, hurt and die.
       - Change rect size when player changes size.
       - Freeze player when hurting, in case they're falling or mid-jump.
       - Fix animations that wouldn't work when player is idle. It was because animSpeed was
         being set to 0 when idle.
    - Collision detection with enemies, to squash them or get hurt.
       - Allow them to walk thru you when invulnerable, but can still squash them from above.
       - Don't rely on collision direction detection, which can be unreliable. Instead, just
         check if we hit the top of the enemy and we are going downwards.
       - Handle jumping on multiple enemies at once without dying. This happens because we
         bounce up after hitting the first enemy, which then kills us for touching the second
         enemy. Instead, we wait a tick until bouncing which ensures all enemies are squashed.
       - Bouncing off enemies sends you a little higher than normal jump

--- src/entities/player.js
+++ src/entities/player.js
@@ -14,22 +14,115 @@ export function makePlayer(pos, options = optionDefaults) {
    const jumpForces = { sm: 1300, lg: 1350 };
    const skidSound = k.play('skid', { paused: true, loop: true, speed: 0.9, volume: 0.6 });
    const runSound = k.play('p-meter', { paused: true, loop: true });
+   let alive = true;
+   let invulnerable = false;
+   let frozen = false;
    let lastPos = pos.clone();
    let runTime = 0;
    let momentum = 0;
+   function makePlayerAreaRect() {
+      return size === 'sm' ? new k.Rect(k.vec2(0, 0), 10, 15) : new k.Rect(k.vec2(0, 0), 13, 27);
+   }
    return k.add([
-      k.sprite(char, { frame: 0 }),
+      k.sprite(char, { frame: 0, flipX: true }),
       k.scale(4),
-      k.area(),
-      k.anchor('center'),
+      k.area({ shape: makePlayerAreaRect() }),
+      k.anchor('bot'),
       k.pos(pos),
       k.body(),
+      k.z(1),
       k.offscreen({ distance: 25 }),
       variableJump(),
+      'player',
       {
+         die() {
+            if (size!=='sm' || !alive || invulnerable) return;
+            this.trigger('die');
+            this.isStatic = true;
+            this.vel = k.vec2(0, 0);
+            this.area.shape = new k.Rect(k.vec2(0, 0), 0, 0);
+            alive = false;
+            this.z = 1000;
+            runSound.stop();
+            skidSound.stop();
+            k.play('die');
+            this.play(`die-${size}`);
+            this.opacity = 1;
+            this.scaleBy(1.2);
+            k.wait(2.6, () => {
+               this.isStatic = false;
+               this.jump(jumpForces.lg);
+            });
+         },
+         get isAlive() {
+            return alive;
+         },
+         get isInvulnerable() {
+            return invulnerable;
+         },
+         set isInvulnerable(val) {
+            invulnerable = val;
+         },
+         grow() {
+            if (size==='lg' || !alive || frozen) return;
+            frozen = true;
+            const vel = this.vel.clone();
+            this.isStatic = true;
+            this.vel = k.vec2(0, 0);
+            k.play('powerup');
+            this.play('grow', { speed: 20 });
+            k.wait(1, ()=>{
+               this.stop();
+               size = 'lg';
+               this.isStatic = false;
+               frozen = false;
+               this.vel = vel.scale(1.1);
+               this.area.shape = makePlayerAreaRect();
+            });
+         },
+         hurt() {
+            if (invulnerable) return;
+            this.trigger('hurt');
+            if (size==='sm') {
+               this.die();
+               return;
+            }
+            invulnerable = true;
+            frozen = true;
+            const vel = this.vel.clone();
+            this.isStatic = true;
+            this.vel = k.vec2(0, 0);
+            k.wait(2.5, ()=>invulnerable = false);
+            k.play('hurt');
+            // TODO: Animation does not play if play is touched by enemy when not moving.
+            this.play('grow', { speed: 20 });
+            k.wait(0.7, ()=>{
+               size = 'sm';
+               this.isStatic = false;
+               this.vel = vel.scale(0.9);
+               this.area.shape = makePlayerAreaRect();
+               frozen = false;
+            });
+         },
+         handleCollideEnemy(enemy, col) {
+            if (!alive || frozen || !enemy.isAlive) return;
+            // Must hit top part of enemy  with downward velocity to squash
+            const thresholdY = enemy.pos.y - enemy.area.shape.pos.y - enemy.area.shape.height / 2;
+            if ((this.pos.y <= thresholdY) && this.vel.y > 0) {
+               enemy.squash();
+               // We wait a tick to bounce in case we squash multiple enemies in one frame
+               k.wait(0, ()=>{
+                  if (this.isJumping()) return;
+                  this.variableJump(jumpForces[size]*1.1);
+               });
+            } else if (!invulnerable) {
+               enemy.freeze(0.7);
+               this.hurt();
+            }
+         },
          add() {
             k.onButtonPress('jump', ()=>{
-               if (!this.isGrounded()) return;
+               if (!this.isGrounded() || !alive || frozen) return;
                const anim = `${this.curAnim()?.startsWith('run') ? 'pjump' : 'jump'}-${size}`;
                if (this.hasAnim(anim)) this.play(anim);
                k.play('jump');
@@ -39,7 +132,24 @@ export function makePlayer(pos, options = optionDefaults) {
                if (runTime>=prunThreshold) jumpForce *= 1.1;
                this.variableJump(jumpForce);
             });
-            k.onUpdate(()=>{
+            this.onBeforePhysicsResolve(col=>{
+               // There's only special logic for touching enemies
+               if (col.target.is('enemy')) {
+                  col.preventResolution();
+                  this.handleCollideEnemy(col.target, col);
+               }
+            });
+            this.onUpdate(()=>{
+               // Don't process if dead
+               if (!alive) return;
+               // Invulnerability
+               if (invulnerable) {
+                  this.opacity = k.wave(0.2, 0.8, k.time() * 75);
+               } else if (this.opacity!==1) {
+                  this.opacity = 1;
+               }
+               // Don't process movement if frozen
+               if (frozen) return;
                // Up/down take priority unless you're mid-jump
                const goUp = this.isGrounded() && k.isButtonDown('up');
                const goDown = !goUp && this.isGrounded() && k.isButtonDown('down');
@@ -59,6 +169,7 @@ export function makePlayer(pos, options = optionDefaults) {
                else if (runTime>prunThreshold) runTime = prunThreshold;
                // Check for p-run
                const prunning = goTurbo && goLeftOrRight && runTime>=prunThreshold;
+               const prunCount = Math.ceil(runTime*6 / prunThreshold);
                const maxSpeed = prunning ? speeds.prun : goTurbo ? speeds.turbo : speeds.walk;
                // Play sound effects
                if (prunning && runSound.paused) runSound.play();
@@ -80,9 +191,9 @@ export function makePlayer(pos, options = optionDefaults) {
                   // TODO: If you let go of turbo mid-jump, this stops them too fast mid-air.
                   if (Math.abs(momentum)>maxSpeed) momentum = maxSpeed * momentumDir;
                }
-               let anim = `${skidding ? 'skid' : prunning ? 'run' : 'walk'}-${size}`;
+               let anim = `${skidding ? 'skid' : prunning ? 'run' : moving ? 'walk' : 'idle'}-${size}`;
                if (goDown && this.hasAnim(`duck-${size}`)) anim = `duck-${size}`;
-               const animSpeed = Math.abs(momentum)/maxSpeed * (goTurbo ? 3 : 1.2);
+               const animSpeed = momentum ? Math.abs(momentum)/maxSpeed * (goTurbo ? 3 : 1.2) : 1;
                // Actually apply calculations to the characters
                lastPos = this.pos.clone();
                if (this.animSpeed!==animSpeed) this.animSpeed = animSpeed;
@@ -90,11 +201,11 @@ export function makePlayer(pos, options = optionDefaults) {
                   this.flipX = momentum>0;
                   this.move(momentum, 0);
                   if (this.isGrounded() && this.hasAnim(anim) && this.curAnim()!==anim) this.play(anim);
-                  if (this.animSpeed!==animSpeed) this.animSpeed = animSpeed;
                } else if (this.isGrounded()) {
-                  if (goDown) this.play(anim);
-                  else if (goUp && this.hasAnim(`lookup-${size}`) && !char.includes('raccoon')) this.play(`lookup-${size}`);
-                  else this.frame = size==='sm' ? 0: 19;
+                  if (goUp && this.hasAnim(`lookup-${size}`) && !char.includes('raccoon')) this.play(`lookup-${size}`);
+                  else this.play(anim);
+               } else if (this.isFalling()) {
+                  this.play(`jump-${size}`);
                }
                // Debug text display
                if (debugText) {
@@ -102,7 +213,7 @@ export function makePlayer(pos, options = optionDefaults) {
                      `Pos: ${this.pos.x.toFixed(0)}, ${this.pos.y.toFixed(0)} (Delta: ${lastPosDelta})\n`+
                      `Momentum: ${momentum}\n`+
                      `Run Time: ${runTime.toFixed(2)}s\n`+
-                     `P-Running: ${prunning}\n`+
+                     `P-Meter: ${'>'.repeat(prunCount) + (prunning ? ' P' : '')}\n`+
                      `Skidding: ${skidding}\n`+
                      `Anim: ${this.curAnim()} (${this.animSpeed.toFixed(1)}x)`;
                }
