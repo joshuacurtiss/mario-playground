8acf20c315473f1aef36dbddc2cb2f09835ca70b
Josh Curtiss <josh@curtiss.me>
2026-05-03 19:02:05 -0500

    chore: Add Piranha to map

--- public/assets/levels/terrain-pipe-green.tsx
+++ public/assets/levels/terrain-pipe-green.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.12" tiledversion="1.12.0" name="terrain-pipe-green" tilewidth="16" tileheight="16" tilecount="6" columns="3">
- <image source="../sprites/terrain-pipe-green.png" width="48" height="32"/>
+<tileset version="1.10" tiledversion="1.12.1" name="terrain-pipe-green" tilewidth="16" tileheight="16" tilecount="10" columns="10">
+ <transformations hflip="1" vflip="0" rotate="1" preferuntransformed="0"/>
+ <image source="../sprites/terrain-pipe-green.png" width="160" height="16"/>
 </tileset>
