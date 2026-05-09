1f6c02c25907285d394dc04d1525e70a1abdc71e
Josh Curtiss <josh@curtiss.me>
2026-05-08 20:44:24 -0500

    feat: Add anchor, rect, rotate type guards

--- src/lib/type-guards.ts
+++ src/lib/type-guards.ts
@@ -1,4 +1,4 @@
-import { GameObj, AreaComp, BodyComp, OpacityComp, PosComp, Rect, Shape } from 'kaplay';
+import { GameObj, AreaComp, BodyComp, OpacityComp, PosComp, Rect, RectComp, Shape, AnchorComp, RotateComp } from 'kaplay';
 
 export function isRect(o: Shape): o is Rect {
    return 'width' in o && 'height' in o;
@@ -8,8 +8,20 @@ export function isGameObjWithArea(obj: GameObj): obj is GameObj<AreaComp> {
    return obj.has('area');
 }
 
+export function isGameObjWithAnchor(obj: GameObj): obj is GameObj<AnchorComp> {
+   return obj.has('anchor');
+}
+
 export function isGameObjWithBody(obj: GameObj): obj is GameObj<BodyComp> {
    return obj.has('body');
+}
+
+export function isGameObjWithRect(obj: GameObj): obj is GameObj<RectComp> {
+   return obj.has('rect');
+}
+
+export function isGameObjWithRotate(obj: GameObj): obj is GameObj<RotateComp> {
+   return obj.has('rotate');
 }
 
 export function isGameObjWithOpacity(obj: GameObj): obj is GameObj<OpacityComp> {
