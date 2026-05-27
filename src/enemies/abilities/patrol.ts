26d02159e36e4a5a84a65e0456152aa2f350ab3e
Josh Curtiss <josh@curtiss.me>
2026-05-26 22:30:44 -0500

    feat: Add ability to enable/disable patrolling

--- src/enemies/abilities/patrol.ts
+++ src/enemies/abilities/patrol.ts
@@ -10,22 +10,26 @@ interface Boundary {
 
 export interface PatrolComp extends Comp {
    get isPatrolling(): boolean;
+   get enablePatrolling(): boolean;
+   set enablePatrolling(newVal: boolean);
 }
 
 export interface PatrolCompOpt {
    pos?: Vec2;
    radius?: number;
    boundary?: Boundary;
+   enabled?: boolean;
 }
 
 const optionDefaults: PatrolCompOpt = {
    pos: k.vec2(0, 0),
    radius: 500,
+   enabled: true,
 };
 
 export function patrol(options: PatrolCompOpt = {}): PatrolComp {
    const opts = Object.assign({}, optionDefaults, options);
-   let { boundary } = opts;
+   let { boundary, enabled } = opts;
    if (!boundary && opts.radius && opts.pos) boundary = {
       left: opts.pos.x - opts.radius,
       right: opts.pos.x + opts.radius,
@@ -35,13 +39,20 @@ export function patrol(options: PatrolCompOpt = {}): PatrolComp {
       id: 'patrol',
       require: [ 'pos', 'freeze', 'move' ],
       get isPatrolling() {
-         return !!boundary;
+         return !!boundary && !!enabled;
+      },
+      get enablePatrolling(): boolean {
+         return enabled ?? false;
+      },
+      set enablePatrolling(newVal: boolean) {
+         enabled = newVal;
       },
       fixedUpdate(this: GameObj<EnemyComps & PatrolComp & MoveComp>) {
          if (this.isFrozen) return;
          if (!this.isGrounded()) return;
          if (!this.pos) return;
          if (!boundary) return;
+         if (!enabled) return;
          if (this.pos.x < boundary.left && this.dir < 0) this.dir = 1;
          if (this.pos.x > boundary.right && this.dir > 0) this.dir = -1;
       }
