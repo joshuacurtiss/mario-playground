a0b0bee5d97fde93e6147afda32bc8288d34237e
Josh Curtiss <josh@curtiss.me>
2026-06-12 21:41:53 -0500

    feat: Add `origPoints` to points comp
    Sometimes an enemy's points are adjusted based on combos. The `origPoints` getter returns
    the original points value set in the beginning. This can be used to definitively get the
    points value to multiply the combo by.

--- src/shared-abilities/points.ts
+++ src/shared-abilities/points.ts
@@ -2,16 +2,21 @@ import { Comp } from 'kaplay';
 
 export interface PointsComp extends Comp {
    get isOneUp(): boolean;
+   get origPoints(): number;
    get points(): number;
    set points(val: number);
 }
 
 export function points(points = 0, threshold1up = 10000): PointsComp {
    let _points = points;
+   const _origPoints = points;
    return {
       id: 'points',
       get isOneUp() {
          return _points > threshold1up;
+      },
+      get origPoints() {
+         return _origPoints;
       },
       get points() {
          return this.isOneUp ? 0 : _points;
